085b9bd6292c6c8404ce73474b5823188a93e5c1
igorbragaia <igor.bragaia@gmail.com>
2019-10-28 16:17:07 -0300

    sql schema fixes

--- app/graphql/resolvers.js
+++ app/graphql/resolvers.js
@@ -38,16 +38,16 @@ export default {
             return simpleQuery( { id: root.user_id }, 'user' ).then( res =>  res !== null ? res[0] : null )
         },
         tags: async ( root, args, context ) => {
-            return innerJoinQuery( { goal_id: root.id }, 'tag', 'goal_tags', 'tag_id' )            
+            return innerJoinQuery( { goal_id: root.id }, 'tag', 'goal_tag', 'tag_id' )            
         },
         tasks: async ( root, args, context ) => {
-            return innerJoinQuery( { goal_id: root.id }, 'task', 'tasks_goals', 'task_id' )            
+            return innerJoinQuery( { goal_id: root.id }, 'task', 'task_', 'task_id' )            
         },
         relateTask: async ( root, args, context ) => {
-            return simpleInsertQuery( { goal_id: root.id, task_id: args.task_id }, 'tasks_goals' ).then( res =>  res !== null && res.length > 0 )
+            return simpleInsertQuery( { goal_id: root.id, task_id: args.task_id }, 'task_goal' ).then( res =>  res !== null && res.length > 0 )
         },
         relateTag: async ( root, args, context ) => {
-            return simpleInsertQuery( { goal_id: root.id, tag_id: args.tag_id }, 'goal_tags' ).then( res =>  res !== null && res.length > 0 )
+            return simpleInsertQuery( { goal_id: root.id, tag_id: args.tag_id }, 'goal_tag' ).then( res =>  res !== null && res.length > 0 )
         }
     }, 
     Task: {
@@ -55,16 +55,16 @@ export default {
             return simpleQuery( { id: root.user_id }, 'user' ).then( res =>  res !== null ? res[0] : null )
         },
         tags: async ( root, args, context ) => {
-            return innerJoinQuery( { task_id: root.id }, 'tag', 'task_tags', 'tag_id' )            
+            return innerJoinQuery( { task_id: root.id }, 'tag', 'task_tag', 'tag_id' )            
         },
         goals: async ( root, args, context ) => {
-            return innerJoinQuery( { task_id: root.id }, 'goal', 'tasks_goals', 'goal_id' )            
+            return innerJoinQuery( { task_id: root.id }, 'goal', 'task_goal', 'goal_id' )            
         },
         relateTag: async ( root, args, context ) => {
-            return simpleInsertQuery( { task_id: root.id, tag_id: args.tag_id }, 'task_tags' ).then( res =>  res !== null && res.length > 0 )
+            return simpleInsertQuery( { task_id: root.id, tag_id: args.tag_id }, 'task_tag' ).then( res =>  res !== null && res.length > 0 )
         },
         relateGoal: async ( root, args, context ) => {
-            return simpleInsertQuery( { task_id: root.id, goal_id: args.goal_id }, 'tasks_goals' ).then( res =>  res !== null && res.length > 0 )
+            return simpleInsertQuery( { task_id: root.id, goal_id: args.goal_id }, 'task_goal' ).then( res =>  res !== null && res.length > 0 )
         }
     },
     Tag: {
@@ -72,10 +72,10 @@ export default {
             return simpleQuery( { id: root.user_id }, 'user' ).then( res =>  res !== null ? res[0] : null )
         },
         tasks: async ( root, args, context ) => {
-            return innerJoinQuery( { tag_id: root.id }, 'task', 'task_tags', 'task_id' )            
+            return innerJoinQuery( { tag_id: root.id }, 'task', 'task_tag', 'task_id' )            
         },
         goals: async ( root, args, context ) => {
-            return innerJoinQuery( { tag_id: root.id }, 'goal', 'goal_tags', 'goal_id' )            
+            return innerJoinQuery( { tag_id: root.id }, 'goal', 'goal_tag', 'goal_id' )            
         }
     }
 }
